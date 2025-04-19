4b10397cb911f3403991761e852b63f4f3668fe0
Thorel Olivier <thorel.olivier@hotmail.com>
2025-04-19 12:44:26 +0200

    ex02

---
+++ ex02/src/Account.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olthorel <olthorel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 12:32:38 by olthorel          #+#    #+#             */
+/*   Updated: 2025/04/19 12:32:38 by olthorel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits; = 0;
+int	Account::_totalNbWithdrawals; = 0;
+
+Account::Account( int initial_deposit ) : 

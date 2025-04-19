3c8419d81d07e294c237f1b1b0c7b3e82987405d
Thorel Olivier <thorel.olivier@hotmail.com>
2025-04-19 15:50:28 +0200

    ex02

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -17,7 +17,83 @@
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
-int	Account::_totalNbDeposits; = 0;
-int	Account::_totalNbWithdrawals; = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
-Account::Account( int initial_deposit ) : 
+Account::Account( int initial_deposit ) : _accountIndex(_nbAccounts++), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+	_totalAmount += initial_deposit;
+}
+
+Account::~Account() {
+	_totalAmount -= _amount;
+}
+
+int	Account::getNbAccounts( void ) {
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ) {
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void ) {
+	return ( _totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return (_totalNbWithdrawals);
+}
+
+int	Account::checkAmount( void ) const {
+	return (_amount);
+}
+
+void	Account::displayAccountsInfos( void ) {
+	_displayTimestamp();
+	std::cout << " account #"       << getNbAccounts();
+	std::cout << ";total:"         << getTotalAmount();
+	std::cout << ";deposits:"      << getNbDeposits();
+	std::cout << ";withdrawals:"   << getNbWithdrawals() << std::endl;
+}
+
+void	Account::displayStatus( void ) const {
+	_displayTimestamp();
+	std::cout << "index:"       << _accountIndex;
+	std::cout << ";amount:"      << _amount;
+	std::cout << ";deposits:"    << _nbDeposits;
+	std::cout << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit( int deposit ) {
+	if (deposit > 0) {
+		_amount += deposit;
+		_nbDeposits++;
+		_totalAmount += deposit;
+		_totalNbDeposits++;
+		displayStatus();
+	}
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+	if (withdrawal <= _amount && withdrawal > 0) {
+		_amount -= withdrawal;
+		_nbWithdrawals++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+		displayStatus();
+		return (true);
+	}
+	std::cout << "Insufficient funds for withdrawal" << std::endl;
+	return (false);
+}
+
+void	Account::_displayTimestamp( void ) {
+	std::time_t now = std::time(0);
+	std::tm *ltm = std::localtime(&now);
+	std::cout << "[" << 1900 + ltm->tm_year;
+	std::cout << std::setw(2) << std::setfill('0') << 1 + ltm->tm_mon;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_mday;
+	std::cout << "_" << std::setw(2) << std::setfill('0') << ltm->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_sec << "] ";
+}

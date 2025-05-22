7bb739eff42821afb2c37b2d6f914a124370eb5c
Olivier Thorel <olthorel@student.42perpignan.fr>
2025-05-22 13:38:19 +0200

    ok module 09

--- Module_09/ex02/src/PmergeMe.cpp
+++ Module_09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: olthorel <olthorel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:22:21 by olthorel          #+#    #+#             */
-/*   Updated: 2025/05/21 14:02:46 by olthorel         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:48:43 by olthorel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,27 +42,21 @@ void PmergeMe::sortVector(std::vector<int>& vec) {
 	size_t n = vec.size();
 	if (n <= 1)
 		return;
-	// Step 1: Compare each pair and make sure the smaller element is first
 	for (size_t i = 0; i + 1 < n; i+=2) {
 		if (vec[i] > vec[i + 1])
 			std::swap(vec[i], vec[i + 1]);
 	}
 	std::vector<int> sorted;
-	// Step 2: Push the smaller element of each pair into the sorted vector
 	for (size_t i = 0; i + 1 < n; i+=2)
 		sorted.push_back(vec[i]);
-	// Step 3: If there's an unpaired last element, add it as well
 	if (n % 2 != 0)
 		sorted.push_back(vec[n - 1]);
-	// Step 4: Sort the initial guide sequence (smaller elements)
 	insertionSortVector(sorted, 0, sorted.size());
-	// Step 5: Insert the larger elements from the pairs into the sorted sequence
 	for (size_t i = 0; i + 1 < n; i+=2) {
 		int value = vec[i + 1];
 		std::vector<int>::iterator pos = std::upper_bound(sorted.begin(), sorted.end(), value);
 		sorted.insert(pos, value);
 	}
-	// Step 6: Replace the original vector with the fully sorted one
 	vec = sorted;
 }
 //Deque version
